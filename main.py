45e15c4569c3f4b557628c68b18ac662389f9466
Anatoliy <anatoliy.klimanov@gmail.com>
2026-02-04 10:30:45 +0300

    v2

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-
 import aiohttp
 import asyncio
 from bs4 import BeautifulSoup
 from sqlalchemy.exc import SQLAlchemyError
 import pandas as pd
-from models import SpimexTradingResult
+from models import SpimexTradingResult, AsyncSessionLocal
+
 
 async def fetch_file_names(session, url):
     # res = ['https://spimex.com/files/55576/', 'https://spimex.com/files/55086/'...]
@@ -15,15 +15,14 @@ async def fetch_file_names(session, url):
             file_names = []
             for a_tag in soup.find_all('a', class_="accordeon-inner__item-title link xls"):
                 href = a_tag.get('href')
-                file_names.append(f"https://spimex.com{href}")
-                file_names.append(f"{href}")
+                file_names.append(href)
             return file_names
     except Exception as e:
         print(f"Ошибка при парсинге списка файлов: {e}")
         return []
 
 
-async def download_file(session, base_url, file_name, db_session):
+async def download_file(session, base_url, file_name, event):
     # на вход base_url = https://spimex.com
     # file_name = '/files/55576/'
     try:
@@ -34,48 +33,44 @@ async def download_file(session, base_url, file_name, db_session):
             with open(f"{split_name_file[2]}.xml", 'wb') as f:
                 f.write(content)
             print(f"Скачан {file_name}")
-            excel_data_df = pd.read_excel(f"{split_name_file[2]}.xml")
+    except asyncio.TimeoutError:
+        print(f"Ошибка скачивания {file_name}")
+    finally:
+        event.set()
+
+
+async def add_to_db(file_name, db_session, event):
+    await event.wait()
+    try:
+        async with db_session:
+            split_name_file = file_name.split('/')
+            excel_data_df = pd.read_excel(split_name_file[1])
             for data in excel_data_df.values:
-                index = 0
-                while index < len(data):
-                    if data[index] == '-' or pd.isna(data[index]):
-                        data[index] = 0
-                    index += 1
-                new_record = SpimexTradingResult(
-                    year = data[0],
-                    мonth_number = data[1],
-                    Category = data[2],
-                    count_contract_petroleum_products = data[3],
-                    value_contract_petroleum_products = data[4],
-                    count_contract_energy_carriers = data[5],
-                    value_contract_energy_carriers = data[6],
-                    count_contract_agricultural = data[7],
-                    value_contract_agricultural = data[8],
-                    count_contract_mineral_raw = data[9],
-                    value_contract_mineral_raw = data[10],
-                    count_contract_oil = data[11],
-                    value_contract_oil = data[12],
-                    count_contract_gas = data[13],
-                    value_contract_gas = data[14],
-                    count_contract_forest = data[15],
-                    value_contract_forest = data[16],
-                    count_contract_metals = data[17],
-                    value_contract_metals = data[18],
-                    count_contract_carbon = data[19],
-                    value_contract_carbon = data[20],
-                    count_contracts = data[21],
-                    count_value_contracts = data[22]
-                )
-                db_session.add(new_record)
-                await db_session.commit()
+                        new_record = SpimexTradingResult(
+                            year = data[0],
+                            month_number = data[1],
+                            category = data[2],
+                            count_contracts = data[-2],
+                            count_value_contracts = data[-1]
+                        )
+                        db_session.add(new_record)
+        await db_session.commit()
     except SQLAlchemyError as e:
         print(f"Ошибка записи в БД для {file_name}: {e}")
         await db_session.rollback()
-    except:
-        print(f"Ошибка скачивания {file_name}, статус {response.status}")
+
 
 async def main(list_page_url, base_url):
-    async with aiohttp.ClientSession() as session:
+    event = asyncio.Event()
+    async with aiohttp.ClientSession() as session, AsyncSessionLocal() as db_session:
         file_names = await fetch_file_names(session, list_page_url)
-        tasks = [download_file(session, base_url, file_name) for file_name in file_names]
-        await asyncio.gather(*tasks)
+        tasks_download_file = [download_file(session, base_url, file_name, event) for file_name in file_names]
+        tasks_add_to_db = [add_to_db(file_name, db_session, event) for file_name in file_names]
+        await asyncio.gather(*tasks_download_file, *tasks_add_to_db)
+
+
+    
+if __name__ == '__main__':
+    list_page_url = 'https://spimex.com/markets/oil_products/trades/results/'
+    base_url = 'https://spimex.com'
+    asyncio.run(main(list_page_url, base_url))
--- migrate.py
+++ migrate.py
@@ -11,9 +11,13 @@ def upgrade():
     op.create_table(
         'spimex_trading_results',
         sa.Column('id', sa.Integer, primary_key=True),
+        sa.Column('year', sa.Float),
+        sa.Column('month_number', sa.Float),
+        sa.Column('category', sa.String),
         sa.Column('count_contracts', sa.Float),
-        sa.Column('count_value_contracts'),
+        sa.Column('count_value_contracts', sa.Float),
     )
+
 
 def downgrade():
     op.drop_table('spimex_trading_results')
